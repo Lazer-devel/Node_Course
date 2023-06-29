5ce21230f35f5ab963c55fb8e8cf23341e43fbbb
Lazer <niklazq@mail.ru>
2023-06-29 14:27:29 +0300

    fix: key in elections map and post /stat

--- homework/1/1/backend/src/index.ts
+++ homework/1/1/backend/src/index.ts
@@ -5,7 +5,7 @@ import cors from "cors";
 console.log("start app");
 const app = express();
 const path = `./src/elections.json`;
-let elections: Map<string, { votes: number }>;
+let elections: Map<string, { name: string; votes: number }>;
 
 app.use(express.json());
 app.use(cors());
@@ -13,6 +13,7 @@ app.use(cors());
 fs.readFile(path, {
   encoding: "utf-8",
 }).then((initData: string) => {
+  console.log(Object.entries(JSON.parse(initData)));
   elections = new Map(Object.entries(JSON.parse(initData)));
   app.listen(10001);
   console.log("Server start listen port 10001");
@@ -20,17 +21,20 @@ fs.readFile(path, {
 
 app.get("/variants", (_, res) => {
   console.log("get /variants");
-  res.json([...elections.keys()]);
+  const variants = [...elections.entries()].map((arr) => {
+    return { id: arr[0], name: arr[1].name };
+  });
+  res.json(variants);
 });
 
-app.get("/stat", (_, res) => {
-  console.log("get /stat");
+app.post("/stat", (_, res) => {
+  console.log("post /stat");
   res.json(Object.fromEntries(elections));
 });
 
 app.post("/vote", (req, res) => {
   console.log("get /vote");
-  elections.get(req.body.name)!.votes++;
+  elections.get(req.body.id)!.votes++;
   fs.writeFile(path, JSON.stringify(Object.fromEntries(elections))).then(() =>
     res.send()
   );
