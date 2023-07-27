5172d5e8f6d8f465a696958730c3267e84dae12b
Lazer <niklazq@mail.ru>
2023-07-27 23:36:27 +0300

    ref postman

--- homework/2/backend/src/index.mjs
+++ homework/2/backend/src/index.mjs
@@ -12,10 +12,12 @@ app.use(express.json())
 app.use(express.static(path.join(__dirname, '../public')))
 
 app.get('/', (req, res) => {
+  console.log('GET / req')
   res.sendFile(path.join(__dirname, '../public/index.html'))
 })
 
 app.post('/fetch', async (req, res) => {
+  console.log('POST /fetch req')
   const { url, method, headers, body } = req.body
 
   try {
