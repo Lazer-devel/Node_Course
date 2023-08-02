06560a1a3faaf463e8c774f0e5abb91ddcc324ac
Lazer <niklazq@mail.ru>
2023-08-02 16:41:11 +0300

    feat: redirect to auth form

--- homework/1/2/backend/src/index.ts
+++ homework/1/2/backend/src/index.ts
@@ -1,5 +1,6 @@
 import express from 'express'
-import { createAuth } from './utils'
+import { engine } from 'express-handlebars'
+
 var path = require('path')
 
 const app = express()
@@ -7,26 +8,36 @@ const app = express()
 const _login: string = 'qwe'
 const _password: string = '123'
 
+app.engine('handlebars', engine())
+app.set('view engine', 'handlebars')
+app.set('views', path.join(__dirname, 'views'))
+
 app.use(express.static(path.resolve('public')))
 app.use(express.urlencoded({ extended: true }))
 
-app.get('/auth', (req, res: any) => {
+app.get('/auth', (req, res) => {
   console.log('GET')
-  if (req.query.login) {
-    const { login, password } = req.query
-    res.send(
-      createAuth(login.toString(), password?.toString(), 'Ошибка авторизации')
-    )
-    return
-  }
-  res.send(createAuth())
+  const { login, password } = req.query
+  res.render('auth-page', {
+    layout: 'auth-layout',
+    login: login?.toString(),
+    password: password?.toString(),
+    err: login ? 'Ошибка авторизации' : ''
+  })
+})
+
+app.get('/successAuth', (req, res) => {
+  console.log('success auth')
+  res.render('success-auth-page', {
+    layout: 'auth-layout'
+  })
 })
 
 app.post('/validate', (req, res) => {
   console.log('POST')
   const { login, password } = req.body
   if (_login === login && _password === password) {
-    res.json(req.body)
+    res.redirect('/successAuth')
     return
   }
   res.redirect(`/auth?login=${login}&password=${password}`)
